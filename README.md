e81b5cd2df366b63ad50cc38b1e22a2f9a9c884c
Anderson Ribeiro <anderson@tresna.co>
2020-02-02 14:46:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# expo-shaker
+Shaker is the simplest way to users give feedback into your react-native aplication.
+
+## Using
+```js
+import Shaker from 'expo-shaker'
+
+export default Shaker(App, {
+  projectId: '[YOUR-PROJECT-ID]'
+})
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+import Shaker from './react-native-shaker-component'
+
+import { Accelerometer } from 'expo-sensors'
+
+const THRESHOLD = 200
+
+function detectShake({ shakeTimes, capture }) {
+  let shakesQuantity = 0
+  let shakeTimeout = null
+  let last_x = null
+  let last_y = null
+  let last_z = null
+  let lastUpdate = 0
+  Accelerometer.addListener(accelerometerData => {
+      let {x, y, z} = accelerometerData;
+      let currTime = Date.now();
+      if ((currTime - lastUpdate) > 100) {
+        let diffTime = (currTime - lastUpdate);
+        lastUpdate = currTime;
+
+        let speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+
+        if ( speed > THRESHOLD ) {
+          if ((shakeTimes - 1) === shakesQuantity) {
+            capture()
+          } else {
+            shakesQuantity += 1
+            if (shakeTimeout) clearTimeout(shakeTimeout)
+            shakeTimeout = setTimeout(() => (shakesQuantity = 0), 1000)
+          }
+        }
+        last_x = x;
+        last_y = y;
+        last_z = z;
+      }
+  });
+}
+
+export default function ShakerExtended (component, params) {
+  return Shaker(component, {
+    ...params,
+    detectShakeFn: detectShake
+  })
+}
